8bfe33d6ba681c83e37aea1a6655906d5d6af92f
Michał Segieta <segieta.michal@gmail.com>
2017-03-27 19:20:57 +0200

    Send new form

---
+++ RosterBuilder/Login.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace RosterBuilder
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginlabel = new System.Windows.Forms.Label();
+            this.passlabel = new System.Windows.Forms.Label();
+            this.loginbtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // loginlabel
+            // 
+            this.loginlabel.AutoSize = true;
+            this.loginlabel.Location = new System.Drawing.Point(131, 171);
+            this.loginlabel.Name = "loginlabel";
+            this.loginlabel.Size = new System.Drawing.Size(55, 13);
+            this.loginlabel.TabIndex = 0;
+            this.loginlabel.Text = "Username";
+            // 
+            // passlabel
+            // 
+            this.passlabel.AutoSize = true;
+            this.passlabel.Location = new System.Drawing.Point(133, 211);
+            this.passlabel.Name = "passlabel";
+            this.passlabel.Size = new System.Drawing.Size(53, 13);
+            this.passlabel.TabIndex = 1;
+            this.passlabel.Text = "Password";
+            // 
+            // loginbtn
+            // 
+            this.loginbtn.Location = new System.Drawing.Point(192, 249);
+            this.loginbtn.Name = "loginbtn";
+            this.loginbtn.Size = new System.Drawing.Size(100, 23);
+            this.loginbtn.TabIndex = 2;
+            this.loginbtn.Text = "Sign In";
+            this.loginbtn.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(192, 168);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(192, 208);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 481);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.loginbtn);
+            this.Controls.Add(this.passlabel);
+            this.Controls.Add(this.loginlabel);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loginlabel;
+        private System.Windows.Forms.Label passlabel;
+        private System.Windows.Forms.Button loginbtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ RosterBuilder/Roster.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RosterBuilder
+{
+    public partial class Roster : Form
+    {
+        public Roster()
+        {
+            InitializeComponent();
+
+            }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void codex_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                switch ((string)armychoice.SelectedItem)
+                {
+                case "Empire":
+                        codex.AppendText("Knight");
+                        codex.AppendText(Environment.NewLine);
+                        codex.AppendText("Swordsman");
+                        codex.AppendText(Environment.NewLine);
+                        break;
+                case "Elves":
+                    codex.AppendText("Fighter");
+                    codex.AppendText(Environment.NewLine);
+                    codex.AppendText("Archer");
+                    codex.AppendText(Environment.NewLine);
+                    break;
+                case "Chaos":
+                    codex.AppendText("Dark Knight");
+                    codex.AppendText(Environment.NewLine);
+                    codex.AppendText("Swordsman");
+                    codex.AppendText(Environment.NewLine);
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+    
+    }
+}
